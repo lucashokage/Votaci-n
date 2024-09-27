c4c7e2ae8228c8993bb8512a9fd248b80bef48b6
lucashokage <lucashokage25@gmail.com>
2024-09-27 07:48:54 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+{
+  "categories": [
+    {
+      "id": 1,
+      "name": "Categoría 1",
+      "options": [
+        {
+          "id": 1,
+          "name": "Opción 1",
+          "image": "opcion1.jpg",
+          "votes": 0
+        },
+        {
+          "id": 2,
+          "name": "Opción 2",
+          "image": "opcion2.jpg",
+          "votes": 0
+        },
+        {
+          "id": 3,
+          "name": "Opción 3",
+          "image": "opcion3.jpg",
+          "votes": 0
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Categoría 2",
+      "options": [
+        {
+          "id": 4,
+          "name": "Opción 4",
+          "image": "opcion4.jpg",
+          "votes": 0
+        },
+        {
+          "id": 5,
+          "name": "Opción 5",
+          "image": "opcion5.jpg",
+          "votes": 0
+        },
+        {
+          "id": 6,
+          "name": "Opción 6",
+          "image": "opcion6.jpg",
+          "votes": 0
+        }
+      ]
+    }
+  ]
+}
